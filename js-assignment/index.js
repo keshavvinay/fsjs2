56190a0bf1591fadcc9802e2736da1a92cd3cdeb
Chelluri Keshav Vinay <keshavvinay1161@gmail.com>
2023-01-22 23:33:01 +0530

    added random colors functionality

--- js-assignment/index.js
+++ js-assignment/index.js
@@ -11,6 +11,8 @@ document.body.addEventListener('click', (event) => {
 
 function bulkElement(element){
     setTimeout(() => {
+        let colorHexCode = generateRandomColor()
+        element.style.backgroundColor = colorHexCode;
         element.classList.add('bulk')
     }, 1000);
 }
@@ -19,4 +21,15 @@ function disappearElement(element){
     setTimeout(() => {
         document.body.removeChild(element)
     }, 2000);
+}
+
+// logic taken from anurag sir class
+function generateRandomColor(){
+    let hex = "0123456789ABCDEF";
+    let hash = "#";
+    for(let i=0; i<6; i++){
+        hash = hash + hex[Math.floor(Math.random() * 16)]
+    }
+    // console.log(hash)
+    return hash;
 }
