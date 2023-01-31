a851b5779cd6456d1c4ac7745513606c1542acb3
Chelluri Keshav Vinay <keshavvinay1161@gmail.com>
2023-01-31 23:06:01 +0530

    dom assignment 2 completed

--- DOM-Assignments/DOM Assignment 2.0 1,2,3/about/about.js
+++ DOM-Assignments/DOM Assignment 2.0 1,2,3/about/about.js
@@ -1,3 +1,16 @@
+//task 2
+let accordianWrapper = document.querySelector('.accordian-wrapper')
+let newAccordianDiv = document.createElement('div');
+let h3 = document.createElement('h3');
+let p = document.createElement('p');
+newAccordianDiv.append(h3);
+h3.innerText = "Skills";
+newAccordianDiv.append(p);
+p.innerText = "I posses a very good command over the Full Stack Development technologies like MERN which can be seen in my work over the Github.";
+p.style.display = 'none';
+newAccordianDiv.classList.add('accordian');
+accordianWrapper.append(newAccordianDiv);
+
 let accordian = document.querySelectorAll(".accordian h3");
 accordian.forEach((element) => {
   element.addEventListener("click", () => {
@@ -8,4 +21,11 @@ accordian.forEach((element) => {
       para.style.display = "block";
     }
   });
+});
+
+//assignment 2
+//task 1
+let allAccordians = document.querySelectorAll('.accordian h3');
+allAccordians.forEach(element => {
+    element.style.backgroundColor = "#DADAF5" 
 });
