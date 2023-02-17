e42b5df8f49c3ec88eb111ad69c11bf626e408e5
Chelluri Keshav Vinay <keshavvinay1161@gmail.com>
2023-02-18 00:21:08 +0530

    dom assignments 7 part2 completed

--- DOM-Assignments/DOM P7/DOM P7/index.js
+++ DOM-Assignments/DOM P7/DOM P7/index.js
@@ -5,3 +5,21 @@ languagesElement.forEach((ele) => {
         ele.remove();
     }
 })
+
+const button = document.createElement('button');
+const div = document.createElement('div');
+const buttonCss = `
+background-color : red;
+color : white;
+padding-block: 18px;
+border : none;
+width : 75%;
+margin : 0 auto;
+font-size: 18px;
+border-radius : 6px;
+`;
+button.innerText = 'After Writing the text submit the form';
+const main = document.querySelector('.main')
+main.append(div)
+main.append(button)
+button.style.cssText = buttonCss;
